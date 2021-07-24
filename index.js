c69c267b93e0f060b90685c8b465e291769c8e22
GabrielTS <peliculasg0990@gmail.com>
2021-07-24 17:31:22 -0300

    spam

--- index.js
+++ index.js
@@ -153,8 +153,27 @@ client.on('message', message => {
 
 
 
-/////////////// bloque palabras ///////////////
+/////////////// server ///////////////
 
+
+if(command === 'server'){
+
+  var server = message.guild;
+
+  const embed = new Discord.RichEmbed()
+  .setThumbnail(server.iconURL)
+  .setAuthor(server.name, server.iconURL)
+  .addField('ID', server.id, true)
+  .addField('Region', server.region, true)
+  .addField('Creado el', server.joinedAt.toDateString(), true)
+  .addField('Dueño del Servidor', server.owner.user.username+'#'+server.owner.user.discriminator+' ('+server.owner.user.id +')', true)
+  .addField('Miembros', server.memberCount, true)
+  .addField('Roles', server.roles.size, true)
+  .setColor(0x66b3ff)
+  
+ message.channel.send({ embed });
+
+}
 
 /////////////// Mensajes Embed ///////////////
 
