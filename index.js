0401e791f28fb89d6ddbbbb50f42ce18e4cc2973
GabrielTS <peliculasg0990@gmail.com>
2021-07-31 20:55:51 -0300

    saludo bn

--- index.js
+++ index.js
@@ -126,13 +126,54 @@ client.on('guildMemberAdd', member => {
   channel.send(`Bienvenido al server, ${member}`);
 });
 
+//ban
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content === 'avatar2') {
-    // Send the user's avatar URL
-    message.reply(message.author.displayAvatarURL());
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('!ban')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+         */
+        member
+          .ban({
+            reason: '¡Eran malos!',
+          })
+          .then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply(`Prohibido con éxito ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('No pude prohibir al miembro');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("¡Esa usuaria no está en este gremio!");
+      }
+    } else {
+      // Otherwise, if no user was mentioned
+      message.reply("¡No mencionaste al usuario para prohibir!");
+    }
   }
 });
 
