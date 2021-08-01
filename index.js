4fe4bf7ce1dae911711dcd8ade63880d5dbf2de9
GabrielTS <peliculasg0990@gmail.com>
2021-07-31 21:19:51 -0300

    saludo bn

--- index.js
+++ index.js
@@ -115,6 +115,9 @@ client.on('message', msg => {
   }
 });
 
+
+                      // Comandos//
+
 // heroku git:clone -a botd00
 
 client.on('guildMemberAdd', member => {
@@ -172,12 +175,57 @@ client.on('message', message => {
       }
     } else {
       // Otherwise, if no user was mentioned
-      message.reply("¡No mencionaste al usuario para prohibir!");
+      message.reply("¡No mencionaste al usuario para Bannear!");
     }
   }
 });
 
+// algo nuevo
 
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Razón opcional que se mostrará en los registros de auditoría.')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Pateado con éxito ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('No pude patear al miembro');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("¡Ese usuario no está en este gremio!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("¡No mencionaste al usuario para patear!");
+    }
+  }
+});
 
 
 ///////////// Enviar el avatar al server ///////////////
