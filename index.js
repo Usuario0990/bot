e0d7fc88c784ffd54bbaac80e541fd91024bf407
GabrielTS <peliculasg0990@gmail.com>
2021-07-31 23:57:59 -0300

    saludo bn

--- index.js
+++ index.js
@@ -175,38 +175,7 @@ client.on('message', message => {
 
 // algo nuevo
 
-if(command === 'user'){
-  let userm = message.mentions.users.first()
-  if(!userm){
-    var user = message.author;
 
-      const embed = new Discord.RichEmbed()
-      .setThumbnail(user.avatarURL)
-      .setAuthor(user.username+'#'+user.discriminator, user.avatarURL)
-      .addField('Jugando a', user.presence.game != null ? user.presence.game.name : "Nada", true)
-      .addField('ID', user.id, true)
-      .addField('Estado', user.presence.status, true)
-      .addField('Apodo', message.member.nickname, true)
-      .addField('Cuenta Creada', user.createdAt.toDateString(), true)
-      .addField('Fecha de Ingreso', message.member.joinedAt.toDateString())
-      .addField('Roles', message.member.roles.map(roles => \${roles.name}``).join(', '))
-      .setColor(0x66b3ff)
-
-     message.channel.send({ embed });
-  }else{
-    const embed = new Discord.RichEmbed()
-    .setThumbnail(userm.avatarURL)
-    .setAuthor(userm.username+'#'+userm.discriminator, userm.avatarURL)
-    .addField('Jugando a', userm.presence.game != null ? userm.presence.game.name : "Nada", true)
-    .addField('ID', userm.id, true)
-    .addField('Estado', userm.presence.status, true)
-    .addField('Cuenta Creada', userm.createdAt.toDateString(), true)
-    .setColor(0x66b3ff)
-
-   message.channel.send({ embed });
-  }
-
-}
 
 ///////////// Enviar el avatar al server ///////////////
 
@@ -366,7 +335,22 @@ client.on('message', message => {
   }
 });
 
+const user = message.author;
+const member = message.member;
 
+const embed = new Discord.MessageEmbed()
+
+    .setAuthor('Username: ' + user.tag, user.avatarURL())
+    .setThumbnail(user.avatarURL())
+    .addField('Creación de la cuenta', user.createdAt.toLocaleDateString(), true)
+    .addField('Estado', user.presence.status, true)
+    .addField('Apodo', member.nickname ? member.nickname : 'No tiene', true)
+    .addField('Roles', 
+        message.member.roles.cache.map(rol => '`' + rol.name + '`').join(', ')
+    )
+    .setFooter('ID: ' + user.id)
+
+message.channel.send({embed});
 
 
 
