a8ece18657f0444f3ec5be9fc78d1c50631e2dd9
GabrielTS <peliculasg0990@gmail.com>
2021-07-31 21:04:13 -0300

    saludo bn

--- index.js
+++ index.js
@@ -177,6 +177,26 @@ client.on('message', message => {
   }
 });
 
+//embed
+
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
+
 
 ///////////// Enviar el avatar al server ///////////////
 
