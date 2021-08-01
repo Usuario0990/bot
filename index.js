0ed27162e1559273e07be9462c745c5bf141ead2
GabrielTS <peliculasg0990@gmail.com>
2021-07-31 23:00:14 -0300

    saludo bn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, MessageEmbed, MessageAttachment } = require('discord.js'); 
-const Discord = require("discord.js");
+
 const client = new Discord.Client();
 const Zeew = require("zeew");
 const config = require('./config/bot.json');
@@ -135,10 +135,7 @@ client.on('message', message => {
   // Ignore messages that aren't from a guild
   if (!message.guild) return;
 
-  // if the message content starts with "!ban"
   if (message.content.startsWith('!ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
     // If we have a user mentioned
     if (user) {
@@ -146,13 +143,6 @@ client.on('message', message => {
       const member = message.guild.member(user);
       // If the member is in the guild
       if (member) {
-        /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         * Read more about what ban options there are over at
-         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
-         */
         member
           .ban({
             reason: '¡Eran malos!',
@@ -162,9 +152,6 @@ client.on('message', message => {
             message.reply(`Se a banneado con éxito a ${user.tag}`);
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to ban the member,
-            // either due to missing permissions or role hierarchy
             message.reply('No puedo bannear al miembro');
             // Log the error
             console.error(err);
@@ -182,50 +169,6 @@ client.on('message', message => {
 
 // algo nuevo
 
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Razón opcional que se mostrará en los registros de auditoría.')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Pateado con éxito ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('No pude patear al miembro');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("¡Ese usuario no está en este gremio!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("¡No mencionaste al usuario para patear!");
-    }
-  }
-});
 
 
 ///////////// Enviar el avatar al server ///////////////
