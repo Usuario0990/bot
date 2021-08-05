4c6b1e2d3ce45e432032cc93a892fe5a251259de
GabrielTS <peliculasg0990@gmail.com>
2021-08-05 01:09:02 -0300

    bn

--- index.js
+++ index.js
@@ -93,21 +93,6 @@ client.on("message", (message) => {
 ////// elimina mensjes
 
 
-if(!message.member.hasPermission("!apodo")){ //Verificamos si user tiene permisos suficientes
-    return message.channel.send("[â] No tienes permisos || **Necesitas permisos de `Gestionar apodos`**") //lo que dice si no tiene permisos
-}
-
-let persona = message.mentions.members.first() //Definimos "persona" como al que le hizo ping
-if(!persona) {
-    return message.channel.send("[â] No hay una persona || **Debes mencionar a una persona**") //lo que dice si no menciono a nadie
-}else if(persona.highestRole.comparePositionTo(message.member.highestRole) > 0){
-    return message.channel.send("[â] No tienes permisos || **Esa persona tiene tu mismo o superior rol**") //lo que dice si esa persona tiene tu mismo o  superior rol *Depende de la jerarquia de discord*
-}
-let apodo = args.slice(1).join(' ') //Defiinimos "apodo" como el nuevo apodo del "persona"
-if(!apodo) return message.channel.send("[â] No hay apodo || **Debes escribir el apodo a cambiar**") //lo que dice si no hay apodo definido
-persona.setNickname(apodo) //El bot hace el cambio del apodo usando "persona" y "apodo"
-message.channel.send(`Listo, el nuevo apodo de ${persona} es **${apodo}**`) //Mensaje diciendo el nuevo apodo de la persona
-}
 
 /////////////// Anuncio Embed //////////////////
 
